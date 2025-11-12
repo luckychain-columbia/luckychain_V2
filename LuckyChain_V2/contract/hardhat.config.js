4bf753c65a6a367ee41ea4701df21b9a89db8677
alaradinc <100169255+alaradinc@users.noreply.github.com>
2025-11-12 15:30:53 -0500

    feat: add hardhat ignition module for lottery

--- LuckyChain_V2/contract/hardhat.config.js
+++ LuckyChain_V2/contract/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ignition-ethers");
+require("@nomicfoundation/hardhat-ignition");
 require("dotenv").config();
 
 const { SEPOLIA_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
